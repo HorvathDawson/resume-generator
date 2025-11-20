98a269bdba62a080076f6b00e5c5083aeed5fec5
dawson horvath <horvath.dawson@gmail.com>
2025-11-19 21:46:36 -0800

    fix pathing?

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // ⬇️ ADD or UPDATE THIS LINE ⬇️
+  base: '/resume-generator/', 
+  
   plugins: [react()],
 })
